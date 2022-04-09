4d25badc25e920073bbdf4c478e193a02e980374
smrutikapatil <smruti.patil17@gmail.com>
2022-04-09 15:28:23 +0530

    Added- Ability to generate premium ride bonus

--- src/main/java/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator.java
@@ -1,32 +1,68 @@
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class CabInvoiceGenerator {
 
-    private static final int COST_PER_MINUTE = 1;
-    private static final double MINIMUM_COST_PER_KM = 10;
-    private static final double MINIMUM_FARE = 5.0;
+    Scanner scanner = new Scanner(System.in);
 
-    //To Calculate total fare
+    // Purpose : To calculate total fare for normal and premium rides
+
     public double calculateFare(double distance, int time) {
+        int costPerMinute = 0;
+        double minimumCostPerKM = 0;
+        double minimumFare = 0;
 
-        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_MINUTE;
+        System.out.println("\nPress 1 - Normal Rides\nPress 2 - Premium Rides");
+        //int choice = 2; -- > To test JUNit for premium rides
+        int choice = scanner.nextInt();
 
-        if(totalFare < MINIMUM_FARE)
-            return MINIMUM_FARE;
-        return totalFare;
-    }
+        if (choice == 1) {
 
-    //To calculate fare for multiple rides
-    public InvoiceSummary calculateFare(Ride[] rides) {
-        double totalAggregateFare = 0;
-        for (Ride ride :rides) {
-            totalAggregateFare += this.calculateFare(ride.distance,ride.time);
+            costPerMinute = 1;
+            minimumCostPerKM = 10;
+            minimumFare = 5;
+
+        } else if (choice == 2) {
+
+            costPerMinute = 2;
+            minimumCostPerKM = 15;
+            minimumFare = 20;
+
+        } else {
+
+            System.out.println("Invalid Input !!! ");
+
         }
-        System.out.println(totalAggregateFare);
-        return new InvoiceSummary(rides.length,totalAggregateFare);
+
+        double totalFare = distance * minimumCostPerKM + time * costPerMinute;
+
+        if (totalFare < minimumFare)
+            return minimumFare;
+
+        return totalFare;
+
     }
 
-    public InvoiceSummary inVoiceService(Hashtable<Integer, Ride[]> listOfRides){
+    // Purpose : To calculate fare for multiple rides
+
+    public InvoiceSummary calculateFare(Ride[] rides) {
+
+        double totalAggregateFare = 0;
+        for (Ride ride : rides) {
+
+            totalAggregateFare += this.calculateFare(ride.distance, ride.time);
+
+        }
+
+        System.out.println(totalAggregateFare);
+        return new InvoiceSummary(rides.length, totalAggregateFare);
+
+    }
+
+    // Purpose : To take user ID and get list of ride and return invoice
+
+    public InvoiceSummary inVoiceService(Hashtable<Integer, Ride[]> listOfRides) {
+
         Ride[] rides = listOfRides.get(1);
         InvoiceSummary inVoiceSummary = calculateFare(rides);
         return inVoiceSummary;
